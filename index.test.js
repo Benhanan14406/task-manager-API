15e189ddc120ea9ffa5c61a5c2359a9114316637
angelobenhanan <angelo.benhanan@ui.ac.id>
2025-07-13 19:42:52 +0700

    feat(talent-growth): Made some changes to the unit tests (Unit test still not correct)

--- index.test.js
+++ index.test.js
@@ -1,21 +1,9 @@
 const request = require('supertest');
-const express = require('express');
-const { PrismaClient } = require('./generated/prisma');
-const { z } = require('zod');
 
-const prisma = new PrismaClient();
-const app = express();
-app.use(express.json());
+const app = require('express')();
 
 describe('Task API', () => {
-    // beforeAll(async () => {
-    //     await prisma.task.deleteMany(); // Clean DB
-    // });
-    //
-    // afterAll(async () => {
-    //     await prisma.$disconnect();
-    // });
-
+    // Successful POST /tasks test
     it('should create a task and return status code 200', async () => {
         const response = await request(app).post('/tasks').send({
             title: 'Task 1',
@@ -28,20 +16,22 @@ describe('Task API', () => {
         expect(response.body.title).toBe('Test 1');
     });
 
-    it('should get all tasks and return status code 200', async () => {
-        const response = await request(app).get('/tasks');
-        expect(response.statusCode).toBe(200);
-        expect(Array.isArray(response.body)).toBe(true);
-    });
-
+    // Unsuccessful POST /tasks test
     it('should return status code 400', async () => {
         const response = await request(app).post('/tasks').send({
-            title: '',
-            priority: 'Medium',
+            title: 123,
+            priority: 'Mid',
             deadline: '13 July 2025',
         });
 
         expect(response.statusCode).toBe(400);
         expect(response.body).toHaveProperty('Invalid input.');
+    });
+
+    // GET /tasks test
+    it('should get all tasks and return status code 200', async () => {
+        const response = await request(app).get('/tasks');
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
     });
 });
